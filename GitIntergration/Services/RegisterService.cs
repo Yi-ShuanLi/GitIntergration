d6473e4031d31eb1d902776d1945f74821fd6b84
周政輝 <leo604leo604@gmail.com>
2025-04-15 13:51:53 +0800

    新增驗證信功能

--- GitIntergration/Services/RegisterService.cs
+++ GitIntergration/Services/RegisterService.cs
@@ -1,4 +1,5 @@
 using GitIntergration.Models;
+using GitIntergration.Utility;
 
 namespace GitIntergration.Services
 {
@@ -9,7 +10,13 @@ namespace GitIntergration.Services
             UserModel userModel = new UserModel(model.Name,model.Account,model.Password,model.Address,"");
             userModel.Account = model.Account;
             userModel.Password = model.Password;
+            SendEmail(model.Account,"帳號註冊認證信","請輸入驗證碼");
             return true;
+        }
+
+        public void SendEmail(string account ,string subject,string content)
+        {
+            Mail.SendEmail(account, subject, content);
         }
     }
 }
