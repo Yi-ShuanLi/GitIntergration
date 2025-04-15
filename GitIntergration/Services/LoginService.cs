bbe4f2c80b2d859198b5ea7dadb865203a1afa00
Yi-ShuanLi <beauty1997724@gmail.com>
2025-04-15 13:39:39 +0800

    login_1_建立一個Login Model 的class 及Login Service 的class

---
+++ GitIntergration/Services/LoginService.cs
@@ -0,0 +1,19 @@
+﻿using GitIntergration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIntergration.Services
+{
+    class LoginService
+    {
+        public void loginAct(String account,String password)
+        {
+            LoginModel loginModel = new LoginModel();
+
+        }
+
+    }
+}
