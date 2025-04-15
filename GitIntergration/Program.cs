e498c33e1a98862e46290d6abf7223eaf0a4842a
Yi-ShuanLi <beauty1997724@gmail.com>
2025-04-15 14:41:28 +0800

    在program寫了很基本的登入判斷

--- GitIntergration/Program.cs
+++ GitIntergration/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GitIntergration.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace GitIntergration
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                LoginService loginService = new LoginService();
+                Console.WriteLine("請輸入帳號：");
+                string account = Console.ReadLine();
+                Console.WriteLine("請輸入密碼：");
+                string password = Console.ReadLine();
+                bool temp = loginService.loginAct(account, password);
+                if (temp == true)
+                {
+                    Console.WriteLine("登入成功!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("登入失敗!");
+                }
+            }           
         }
     }
 }
