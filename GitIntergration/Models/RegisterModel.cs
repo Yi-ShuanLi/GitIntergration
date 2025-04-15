4a22851beb94ba19e32a524d4f9c1a9070d355b5
周政輝 <leo604leo604@gmail.com>
2025-04-15 13:35:26 +0800

    註冊功能建制

--- GitIntergration/Models/RegisterModel.cs
+++ GitIntergration/Models/RegisterModel.cs
@@ -1,7 +1,21 @@
+using System;
+
 namespace GitIntergration.Models
 {
     public class RegisterModel
     {
-        
+        public String Account { get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+
+
+        public RegisterModel(String Name,String Account,String Password,String Address)
+        {
+            this.Name = Name;
+            this.Address = Address;
+            this.Account = Account;
+            this.Password = Password;
+        }
     }
 }
